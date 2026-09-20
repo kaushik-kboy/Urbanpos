824902b294933d5bbd727e5e049374919a72b79e
ankitchandak88 <ankitchandak88@gmail.com>
2026-09-21 01:30:17 +0530

    fix(ui): ensure pure black sidebar menu links, direct /create routes for sales and purchase, and bump theme cache buster

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/urbanpets-theme.css') }}?v=20260921_1">
+    <link rel="stylesheet" href="{{ asset('css/urbanpets-theme.css') }}?v=20260921_2">
     @stack('css')
     @yield('css')
     @if(request()->query('is_iframe') == '1')
